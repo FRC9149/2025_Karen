7185bd084aeaf6b8ad21f324b6d631c32179f4e1
9149 PC <frc9149@rfsd.k12.wi.us>
2025-01-08 19:31:47 -0600

    removed tankdrive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,12 +8,9 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.TankDriveTrain;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj.XboxController;
-import main.java.frc.robot.commands.TankDrive;
-import main.java.frc.robot.subsystems.TankDriveTrain;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -27,14 +24,11 @@ public class RobotContainer {
     new XboxController(OperatorConstants.kDriverControllerPort);
 
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  private final TankDriveTrain m_tankDriveTrain = new TankDriveTrain(m_driverController, new Pair(true, false));
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
-    //adds tankdrive as the default
-    m_tankDriveTrain.setDefaultCommand(new TankDrive(m_tankDriveTrain));
   }
 
   /**
