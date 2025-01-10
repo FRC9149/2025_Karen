073f65d04d834bd8d52c0b0430ae3a0b10881bf2
The-Grim-Gambit <riverwahus8@gmail.com>
2025-01-09 19:58:29 -0600

    Update LiftSubsystem.java

--- src/main/java/frc/robot/subsystems/LiftSubsystem.java
+++ src/main/java/frc/robot/subsystems/LiftSubsystem.java
@@ -7,7 +7,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class AlgaeAffecterSubsystem extends SubsystemBase {
+public class LiftSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
   public LiftSubsystem() {}
 
